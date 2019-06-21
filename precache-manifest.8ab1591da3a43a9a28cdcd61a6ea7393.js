3cf8f031a44803328817f134151227492461d3a4
spanaik <span_25862@yahoo.co.in>
2019-06-21 20:40:07 +0530

    Updates

---
+++ precache-manifest.8ab1591da3a43a9a28cdcd61a6ea7393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f320918de1c5cde8057816d42856b514",
+    "url": "/Bank-Detail-Lookup/index.html"
+  },
+  {
+    "revision": "dac18bf2299f2148c5eb",
+    "url": "/Bank-Detail-Lookup/static/css/main.2fe7ecfb.chunk.css"
+  },
+  {
+    "revision": "6aa904ba62e626c95033",
+    "url": "/Bank-Detail-Lookup/static/js/2.42ed585e.chunk.js"
+  },
+  {
+    "revision": "dac18bf2299f2148c5eb",
+    "url": "/Bank-Detail-Lookup/static/js/main.c482223a.chunk.js"
+  },
+  {
+    "revision": "1456b054e6a3dc16f25e",
+    "url": "/Bank-Detail-Lookup/static/js/runtime~main.7b325b12.js"
+  }
+]);
